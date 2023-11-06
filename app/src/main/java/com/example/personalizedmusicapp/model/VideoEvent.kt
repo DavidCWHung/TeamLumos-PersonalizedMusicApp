9128cd32a5f0e4493dde966e7798b519e32dc7c0
FelixChong2022 <105020676+FelixChong2022@users.noreply.github.com>
2023-11-06 11:27:00 -0600

    Updated icon button on Test screen; Moved the add video function from Test to Favourite screen; Added video playback to the favourite list; Added whitespace to the end of the lazycolumn to improve the scrolling.

--- app/src/main/java/com/example/personalizedmusicapp/model/VideoEvent.kt
+++ app/src/main/java/com/example/personalizedmusicapp/model/VideoEvent.kt
@@ -3,13 +3,10 @@ package com.example.personalizedmusicapp.model
 import com.example.personalizedmusicapp.room.Video
 
 sealed interface VideoEvent {
-
     object SaveVideo: VideoEvent
     data class SetYoutubeId(val youtubeId: String): VideoEvent
     object ShowDialog: VideoEvent
     object HideDialog: VideoEvent
     data class DeleteVideo (val video: Video): VideoEvent
-    // object Fav: VideoEvent
     data class DeleteVideoByYoutubeId (val youtubeId: String): VideoEvent
-    // data class CheckFavVideo (val youtubeId: String) : VideoEvent
 }
--- app/src/main/java/com/example/personalizedmusicapp/screen/FavouritesScreen.kt
+++ app/src/main/java/com/example/personalizedmusicapp/screen/FavouritesScreen.kt
@@ -6,16 +6,24 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,61 +32,75 @@ import com.example.personalizedmusicapp.data.Item
 import com.example.personalizedmusicapp.model.VideoEvent
 import com.example.personalizedmusicapp.model.VideoState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavouritesScreen(
     state: VideoState,
-    onEvent: (VideoEvent) -> Unit
-) {
-    LazyColumn(
-        contentPadding = PaddingValues(16.dp),
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    )
-    {
-        items(state.videos) { video ->
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Column(
-                    modifier = Modifier.weight(1f)
-                ) {
+    onEvent: (VideoEvent) -> Unit) {
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center) {
                     Text(
-                        text = "${video.youtubeId}",
-                        fontSize = 20.sp
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Library"
                     )
                 }
-                    IconButton(onClick = {
-                        onEvent(VideoEvent.DeleteVideo(video))
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = "Delete video"
-                        )
-                    }
             }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                onEvent(VideoEvent.ShowDialog)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add contact" )
+            }
+        },
+    )
+    { _ ->
+        if (state.isAddingVideo) {
+            AddVideoDialog(state = state, onEvent = onEvent)
+        }
+        LazyColumn(
+            contentPadding = PaddingValues(16.dp),
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(state.videos) { video ->
+                FavItemCard(youtubeId = video.youtubeId , onEvent = onEvent)
+            }
+            item { Row(modifier = Modifier.height(120.dp)){} }
         }
     }
 }
 
 @Composable
-fun FavItemCard(item: Item, state: VideoState, onEvent: (VideoEvent) -> Unit) {
+fun FavItemCard(youtubeId: String, onEvent: (VideoEvent) -> Unit) {
 
     OutlinedCard(
         modifier = Modifier
             .fillMaxWidth()
             .padding(5.dp)
     ) {
-        Text(item.snippet.title)
-        Text(item.snippet.position)
-        Text(item.snippet.resourceId.videoId)
-        YoutubePlayer(youtubeVideoId = item.snippet.resourceId.videoId)
-        IconButton(onClick = {
-        onEvent(VideoEvent.DeleteVideoByYoutubeId(item.snippet.resourceId.videoId))
-        }) {
-        Icon(
-            imageVector = Icons.Default.Delete,
-            contentDescription = "Delete video"
-        )
+        Row(modifier = Modifier.fillMaxWidth().padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically){
+            Text(youtubeId)
+            Row(modifier= Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End) {
+                IconButton(onClick = {
+                    onEvent(VideoEvent.DeleteVideoByYoutubeId(youtubeId))
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete video"
+                    )
+                }
+            }
         }
+        YoutubePlayer(youtubeVideoId = youtubeId)
     }
 }
