e73da2f46e0e46944c70068d19f8dbb82681093a
DavidCWHung <david.cw.hung@gmail.com>
2023-11-15 14:44:51 -0600

    Replace HomeScreen with TestScreen

--- app/src/main/java/com/example/personalizedmusicapp/MainActivity.kt
+++ app/src/main/java/com/example/personalizedmusicapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.personalizedmusicapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -10,42 +9,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -55,18 +41,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
-import com.example.personalizedmusicapp.data.Item
 import com.example.personalizedmusicapp.data.PlayListItemsResponse
 import com.example.personalizedmusicapp.model.VideoViewModel
 import com.example.personalizedmusicapp.room.VideoDatabase
 import com.example.personalizedmusicapp.screen.FavouritesScreen
-import com.example.personalizedmusicapp.screen.TestScreen
+import com.example.personalizedmusicapp.screen.HomeScreen
 import com.example.personalizedmusicapp.ui.theme.PersonalizedMusicAppTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -80,64 +61,20 @@ interface ApiService {
     ): Response<PlayListItemsResponse>
 }
 
-@Composable
-fun HomeScreen() {
-    var playListItems by remember { mutableStateOf(emptyList<Item>()) }
-    val coroutineScope = rememberCoroutineScope()
-
-    LaunchedEffect(key1 = Unit) {
-        coroutineScope.launch(Dispatchers.IO) {
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://www.googleapis.com/youtube/v3/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
-            val apiService = retrofit.create(ApiService::class.java)
-
-            val part = "snippet"
-            val maxResults = "50"
-            val playlistId = "PL9JwhzITbbGZGA5qjHDbVfNQnK5Sc_XWG"
-            val key = "AIzaSyBKxF26cbuvhSHdc0otnKePjQMi4MLp5GQ"
-
-            val response = apiService.getPlaylistItems(part, maxResults, playlistId, key)
-
-            if (response.isSuccessful) {
-                val responseBody = response.body()
-                if (responseBody != null) {
-                    playListItems = responseBody.items
-                }
-                Log.d("MyApp", "Success")
-            } else {
-                // Handle API error here
-                Log.d("MyApp", "Failed to retrieve!")
-            }
-        }
-    }
-
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(20.dp),
-    ) {
-        items(playListItems) { item ->
-            ItemCard(item)
-        }
-    }
-}
-
-@Composable
-fun ItemCard(item: Item) {
-    OutlinedCard(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(5.dp)
-    ) {
-        Text(item.snippet.title)
-        Text(item.snippet.position)
-        Text(item.snippet.resourceId.videoId)
-
-        YoutubePlayer(youtubeVideoId = item.snippet.resourceId.videoId)
-    }
-}
+//@Composable
+//fun ItemCard(item: Item) {
+//    OutlinedCard(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(5.dp)
+//    ) {
+//        Text(item.snippet.title)
+//        Text(item.snippet.position)
+//        Text(item.snippet.resourceId.videoId)
+//
+//        YoutubePlayer(youtubeVideoId = item.snippet.resourceId.videoId)
+//    }
+//}
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -181,10 +118,9 @@ class MainActivity : ComponentActivity() {
                             navController = navController,
                             startDestination = "home"
                         ) {
-                            composable("home") { HomeScreen() }
+                            composable("home") { HomeScreen(state = state, onEvent=viewModel::onEvent)}
                             composable("search") { SearchScreen() }
                             composable("favourites") { FavouritesScreen(state=state, onEvent=viewModel::onEvent) }
-                            composable("test") { TestScreen(state = state, onEvent=viewModel::onEvent)}
                         }
                     }
                 }
@@ -206,7 +142,6 @@ fun BottomNavigationBar(navController: NavController) {
         BottomNavItem("Home", Icons.Filled.Home, Icons.Outlined.Home, "home"),
         BottomNavItem("Search", Icons.Filled.Search, Icons.Outlined.Search, "search"),
         BottomNavItem("Favourites", Icons.Filled.Favorite, Icons.Outlined.FavoriteBorder, "favourites"),
-        BottomNavItem("Test", Icons.Filled.Warning, Icons.Outlined.Warning, "test")
     )
 
     NavigationBar(
@@ -257,19 +192,3 @@ fun SearchScreen() {
         )
     }
 }
-/*
-@Composable
-fun FavouritesScreen() {
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Favourites Screen",
-            fontWeight = FontWeight.ExtraBold,
-            fontSize = 34.sp
-        )
-    }
-}
-*/
